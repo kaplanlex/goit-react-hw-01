e4141b32e9168ebc87257b11760d48e70ec9443e
AlexKaplan <alexkaplan110606@gmail.com>
2025-01-01 15:18:49 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import userData from "../userData.json";
 import friends from "../friends.json";
 import Profile from "./Profile/Profile";
 import FriendList from "./FriendList/FriendList";
-
+import transactions from "../transactions.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 const App = () => {
     return (
         <>
@@ -14,6 +15,7 @@ const App = () => {
                 stats={userData.stats}
             />
             <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </>
     );
 };
